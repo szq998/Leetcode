02d59ffa07d2a8b7db71f90c51494953b3445211
szq <szq001@foxmail.com>
2021-04-22 19:01:29 +0800

    排除开头重复元素后，二分查找旋转点

--- test.cpp
+++ test.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 #include <vector>
 using std::vector;
-#include "81.搜索旋转排序数组-ii.cpp"
+#include "154.寻找旋转排序数组中的最小值-ii.cpp"
 
 int main() {
-    vector<int> input{3, 2};
-    int target = 2;
+    vector<int> input{3,2,3};
+    // int target = 2;
     Solution *tester = new Solution();
-    string result = tester->search(input, target) ? "true" : "false";
+    // string result = tester->search(input, target) ? "true" : "false";
+    int result = tester->findMin(input);
     cout << result << endl;
 }
