f506ddd8e280f4c2cdb8161cf6466024ae5ea605
szq <szq001@foxmail.com>
2021-04-26 23:16:20 +0800

    快排+双指针

---
+++ 350.两个数组的交集-ii.cpp
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode.cn id=350 lang=cpp
+ *
+ * [350] 两个数组的交集 II
+ */
+
+// @lc code=start
+class Solution {
+    void _quick_recursive(vector<int> &nums, const int low, const int high) {
+        if (high - low < 1)
+            return;
+
+        int pivot = nums[low], l = low, h = high;
+        while (l < h) {
+            while (l < h && nums[h] >= pivot)
+                --h;
+            nums[l] = nums[h];
+            while (l < h && nums[l] <= pivot)
+                ++l;
+            nums[h] = nums[l];
+        }
+        nums[l] = pivot;
+        this->_quick_recursive(nums, low, l - 1);
+        this->_quick_recursive(nums, l + 1, high);
+    }
+    void quick_sort(vector<int> &nums) {
+        this->_quick_recursive(nums, 0, nums.size() - 1);
+    }
+
+public:
+    vector<int> intersect(vector<int> &nums1, vector<int> &nums2) {
+        // 双指针
+        // 排序
+        this->quick_sort(nums1);
+        this->quick_sort(nums2);
+        // 双指针
+        vector<int> inter;
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size();) {
+            if (nums1[i] == nums2[j]) {
+                inter.push_back(nums1[i]);
+                ++i, ++j;
+            } else if (nums1[i] < nums2[j]) {
+                ++i;
+            } else {
+                ++j;
+            }
+        }
+        return inter;
+    }
+};
+// @lc code=end
--- test.cpp
+++ test.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 #include <vector>
 using std::vector;
-#include "349.两个数组的交集.cpp"
+#include "350.两个数组的交集-ii.cpp"
 
 int main() {
-    vector<int> input1{2, 1};
+    vector<int> input1{2, 1, 1};
     vector<int> input2{1, 1};
     // int target = 2;
     Solution *tester = new Solution();
     // string result = tester->search(input, target) ? "true" : "false";
-    auto result = tester->intersection(input1, input2);
-    for (int i = 0; i < result.size(); ++i) {
-        cout << result[i] << ", " << endl;
+    auto result = tester->intersect(input1, input2);
+    for (int &num : result) {
+        cout << num << " ";
     }
+    cout << endl;
 }
