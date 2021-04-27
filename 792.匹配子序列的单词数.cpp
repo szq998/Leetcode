296427f653cb60cbdf11a9910d0147fed0f0536d
szq <szq001@foxmail.com>
2021-04-27 23:14:46 +0800

    不再超时，但还是较慢

--- 792.匹配子序列的单词数.cpp
+++ 792.匹配子序列的单词数.cpp
@@ -6,7 +6,6 @@
 
 // @lc code=start
 #include <unordered_set>
-using std::unordered_set;
 
 class Solution {
     bool testCharInclude(unordered_set<char> &characters, string &word) {
@@ -37,7 +36,41 @@ class Solution {
     }
 
 public:
+    // 只遍历一次s
     int numMatchingSubseq(string s, vector<string> &words) {
+        struct WordWithIdx {
+            string word;
+            int idx = 0;
+            inline char curr_char() { return word[idx]; }
+        };
+        // 按首字母，将words归类
+        vector<vector<WordWithIdx>> categorized_words(26);
+        for (string &word : words) {
+            categorized_words[word[0] - 'a'].push_back({word});
+        }
+        int match_count = 0;
+        for (char &c : s) {
+            // 当前字符对应的words
+            auto i_words = categorized_words[c - 'a'];
+            // 建立新容器
+            categorized_words[c - 'a'] = vector<WordWithIdx>();
+            for (auto &w : i_words) {
+                // 指向word中的下一个字符
+                ++w.idx;
+                if (w.word.size() == w.idx) {
+                    // 指向了word的尾后，表示匹配成功
+                    ++match_count;
+                } else {
+                    // 根据word当前idx指向的字符，归如新类中
+                    categorized_words[w.curr_char() - 'a'].push_back(w);
+                }
+            }
+        }
+        return match_count;
+    }
+
+    // 此方法多次遍历s，速度过慢，复杂度为o(len(s)*len(words)+sigma_i(len(words[i])))
+    int numMatchingSubseq_0(string s, vector<string> &words) {
         // 记录s中的所有字符
         unordered_set<char> characters;
         for (char &c : s) {
